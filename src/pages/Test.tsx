7d6fa0e146a24eddb12b826dda23857f1deec559
davidnan190 <david.nan@msg.group>
2024-07-25 17:02:03 +0300

    [ch5] - migrate styling to SCSS.

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,7 +1,7 @@
 import ProductList from '../components/products/ProductList';
 import { productsMock } from '../mocks/product.mock';
 
-const Test = () => {
+const Test: React.FC = () => {
   return (
     <>
       <h1 className="headline">msg Romania Online Shop</h1>
